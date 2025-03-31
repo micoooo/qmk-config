a8372243cfd43c741f47f00c47c539fc59f894f7
Michael Chung <michaelchungng@live.com>
2025-03-31 21:16:37 +1300

    Add eager debounce

--- keyboards/contra/keymaps/micoo/rules.mk
+++ keyboards/contra/keymaps/micoo/rules.mk
@@ -2,3 +2,9 @@ CONVERT_TO=helios
 
 # Enable Tap Dance
 TAP_DANCE_ENABLE = yes
+
+# Scanning frequency (measured with DEBUG_MATRIX_SCAN_RATE, minimum while
+ # typing):
+ # sym_eager_pr: 990Hz
+ # asym_eager_defer_pk: 955Hz
+DEBOUNCE_TYPE = asym_eager_defer_pk
--- keyboards/planck/rev7/keymaps/ppy/keymap.c
+++ keyboards/planck/rev7/keymaps/ppy/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "common.h"
+#include "quantum.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_planck_grid(
