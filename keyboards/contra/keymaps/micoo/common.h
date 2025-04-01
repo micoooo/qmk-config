61d69dd2e3f98a2c2c8d0114e120b458939be7b5
Michael Chung <michaelchungng@live.com>
2025-04-01 22:35:56 +1300

    Add CleanShot macros

--- keyboards/contra/keymaps/micoo/common.h
+++ keyboards/contra/keymaps/micoo/common.h
@@ -19,3 +19,11 @@ enum planck_keycodes {
 };
 
 #define FKEYS MO(_FKEYS)
+
+#define CUT   LGUI(KC_X)
+#define COPY  LGUI(KC_C)
+#define PASTE LGUI(KC_V)
+
+// CleanShot Macros
+#define CAP_IMG LGUI(LSFT(KC_4))        // Capture portion of screen
+#define CAP_MOV LGUI(LSFT(KC_5))        // Capture video of screen
